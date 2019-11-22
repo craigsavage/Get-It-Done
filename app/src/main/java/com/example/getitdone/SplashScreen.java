eef142a245f629acc5d3c69b9e99b43024c9c336
Craig Savage <craig.savage@ontariotechu.net>
2019-11-22 13:38:43 -0500

    Continued work on the splash screen

--- app/src/main/java/com/example/getitdone/SplashScreen.java
+++ app/src/main/java/com/example/getitdone/SplashScreen.java
@@ -2,13 +2,39 @@ package com.example.getitdone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashScreen extends AppCompatActivity {
+
+    ImageView ivGetItDoneLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        ivGetItDoneLogo = findViewById(R.id.ivGetItDoneLogo);
+
+        Animation animation = AnimationUtils.loadAnimation(SplashScreen.this, R.anim.transitions);
+        ivGetItDoneLogo.startAnimation(animation);
+
+        final Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
     }
 }
