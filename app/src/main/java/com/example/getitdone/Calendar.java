f0f59ae05cf5da9e9c08f7b460c67dc4a7043b92
paivi1 <ryujikomaipro@gmail.com>
2019-11-22 15:05:34 -0500

    Added swipe function to calendar/task list with up/down swipes to transition respectively

--- app/src/main/java/com/example/getitdone/Calendar.java
+++ app/src/main/java/com/example/getitdone/Calendar.java
@@ -1,12 +1,17 @@
 package com.example.getitdone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MotionEventCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class Calendar extends AppCompatActivity {
+    public float touchStart;
+    public float touchEnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +19,52 @@ public class Calendar extends AppCompatActivity {
         setContentView(R.layout.activity_calendar);
         getSupportActionBar().hide();
     }
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+
+        int action = MotionEventCompat.getActionMasked(event);
+
+        switch(action) {
+            case (MotionEvent.ACTION_DOWN) :
+                touchStart = 0;
+                touchEnd = 0;
+                Log.d("Ryuji","Action was DOWN");
+                touchStart = event.getY();
+//                String str = Float.toString(touchStart);
+//                Log.d("Ryuji",str);
+                return true;
+            case (MotionEvent.ACTION_MOVE) :
+                Log.d("Ryuji","Action was MOVE");
+                return true;
+            case (MotionEvent.ACTION_UP) :
+                Log.d("Ryuji","Action was UP");
+                touchEnd = event.getY();
+                if(touchStart-touchEnd > 200){
+                    swipeUp();
+                }
+//                String str2 = Float.toString(touchStart);
+//                Log.d("Ryuji",str2);
+//                String str3 = Float.toString(touchStart-touchEnd);
+//                Log.d("Ryuji",str3);
+                return true;
+            case (MotionEvent.ACTION_CANCEL) :
+                Log.d("Ryuji","Action was CANCEL");
+                return true;
+            case (MotionEvent.ACTION_OUTSIDE) :
+                Log.d("Ryuji","Movement occurred outside bounds " +
+                        "of current screen element");
+                return true;
+            default :
+                return super.onTouchEvent(event);
+        }
+    }
 
     public void swipeUp(View v){
+        Intent intent = new Intent(this, TaskList.class);
+        startActivity(intent);
+        overridePendingTransition (R.anim.up_slide_in, R.anim.up_slide_out);//Enter new, Exit old
+    }
+    public void swipeUp(){
         Intent intent = new Intent(this, TaskList.class);
         startActivity(intent);
         overridePendingTransition (R.anim.up_slide_in, R.anim.up_slide_out);//Enter new, Exit old
--- app/src/main/java/com/example/getitdone/TaskList.java
+++ app/src/main/java/com/example/getitdone/TaskList.java
@@ -1,12 +1,17 @@
 package com.example.getitdone;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MotionEventCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class TaskList extends AppCompatActivity {
+    public float touchStart;
+    public float touchEnd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +19,52 @@ public class TaskList extends AppCompatActivity {
         setContentView(R.layout.activity_task_list);
         getSupportActionBar().hide();
     }
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+
+        int action = MotionEventCompat.getActionMasked(event);
+
+        switch(action) {
+            case (MotionEvent.ACTION_DOWN) :
+                touchStart = 0;
+                touchEnd = 0;
+                Log.d("Ryuji","Action was DOWN");
+                touchStart = event.getY();
+//                String str = Float.toString(touchStart);
+//                Log.d("Ryuji",str);
+                return true;
+            case (MotionEvent.ACTION_MOVE) :
+                Log.d("Ryuji","Action was MOVE");
+                return true;
+            case (MotionEvent.ACTION_UP) :
+                Log.d("Ryuji","Action was UP");
+                touchEnd = event.getY();
+                if(touchStart-touchEnd < -200){
+                    swipeDown();
+                }
+//                String str2 = Float.toString(touchStart);
+//                Log.d("Ryuji",str2);
+//                String str3 = Float.toString(touchStart-touchEnd);
+//                Log.d("Ryuji",str3);
+                return true;
+            case (MotionEvent.ACTION_CANCEL) :
+                Log.d("Ryuji","Action was CANCEL");
+                return true;
+            case (MotionEvent.ACTION_OUTSIDE) :
+                Log.d("Ryuji","Movement occurred outside bounds " +
+                        "of current screen element");
+                return true;
+            default :
+                return super.onTouchEvent(event);
+        }
+    }
 
     public void goToCal(View v){
+        Intent intent = new Intent(this, Calendar.class);
+        startActivity(intent);
+        overridePendingTransition (R.anim.down_slide_in, R.anim.down_slide_out);//Enter new, Exit old
+    }
+    public void swipeDown(){
         Intent intent = new Intent(this, Calendar.class);
         startActivity(intent);
         overridePendingTransition (R.anim.down_slide_in, R.anim.down_slide_out);//Enter new, Exit old
