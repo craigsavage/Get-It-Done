2d11d9bc6b3c117290ad919c23641346701d3f0a
paivi1 <ryujikomaipro@gmail.com>
2019-11-22 13:47:40 -0500

    Removed android toolbar at top and created basic layout for calendar

--- app/src/main/java/com/example/getitdone/Calendar.java
+++ app/src/main/java/com/example/getitdone/Calendar.java
@@ -10,5 +10,6 @@ public class Calendar extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+        getSupportActionBar().hide();
     }
 }
