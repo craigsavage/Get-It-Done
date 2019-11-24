866dce0629cb06aa880dadef6162c30c1d805293
Craig Savage <craig.savage@ontariotechu.net>
2019-11-24 16:58:18 -0500

    working on profile pages

--- app/src/main/java/com/example/getitdone/ProfileSettings.java
+++ app/src/main/java/com/example/getitdone/ProfileSettings.java
@@ -2,7 +2,9 @@ package com.example.getitdone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfileSettings extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class ProfileSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_settings);
+    }
+
+    public void saveProfileInfo(View v) {
+        Intent intent = new Intent(ProfileSettings.this, ProfilePage.class);
+        startActivity(intent);
+        finish();
     }
 }
