e5eef9936868db168b51b72d26d5a9d35a79c368
paivi1 <ryujikomaipro@gmail.com>
2019-11-24 16:37:12 -0500

    msg

--- app/src/main/java/com/example/getitdone/NewTaskDetails.java
+++ app/src/main/java/com/example/getitdone/NewTaskDetails.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class NewTaskDetails extends AppCompatActivity {
 
@@ -18,5 +19,25 @@ public class NewTaskDetails extends AppCompatActivity {
         Intent intent = new Intent(this, TaskList.class);
         startActivity(intent);
         overridePendingTransition (R.anim.left_slide_in, R.anim.left_slide_out);//Enter new, Exit old
+    }
+
+    public void goToTemplates(View v){
+        Intent intent = new Intent(this, NewTask.class);
+        startActivity(intent);
+        overridePendingTransition (R.anim.left_slide_in, R.anim.left_slide_out);//Enter new, Exit old
+    }
+
+    public void imageSwap(View v){
+        ImageView importanceOpt = (ImageView)findViewById(R.id.imageView5);
+        if(importanceOpt.getDrawable().getConstantState() == getResources().getDrawable( R.drawable.barmoderate).getConstantState()){
+            importanceOpt.setImageResource(R.drawable.barhigh);
+        }
+        else if(importanceOpt.getDrawable().getConstantState() == getResources().getDrawable( R.drawable.barhigh).getConstantState()){
+            importanceOpt.setImageResource(R.drawable.barlow);
+        }
+        else if(importanceOpt.getDrawable().getConstantState() == getResources().getDrawable( R.drawable.barlow).getConstantState()){
+            importanceOpt.setImageResource(R.drawable.barmoderate);
+        }
+
     }
 }
