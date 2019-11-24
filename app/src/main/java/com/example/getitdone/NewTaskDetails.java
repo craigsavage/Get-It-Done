490f5889f47a43e96907ccf4366c136135133a3f
paivi1 <ryujikomaipro@gmail.com>
2019-11-23 20:35:28 -0500

    Pushed just 4 Craig

---
+++ app/src/main/java/com/example/getitdone/NewTaskDetails.java
@@ -0,0 +1,14 @@
+package com.example.getitdone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewTaskDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_task_details);
+    }
+}
