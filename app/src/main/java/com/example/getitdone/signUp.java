0a3551d313590b039bb5c6f8c1f9f6c9abfd1ca3
Craig Savage <craig.savage@ontariotechu.net>
2019-11-24 18:09:23 -0500

    fixed layouts and added functionality to add images to profile

--- app/src/main/java/com/example/getitdone/signUp.java
+++ app/src/main/java/com/example/getitdone/signUp.java
@@ -3,16 +3,56 @@ package com.example.getitdone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class signUp extends AppCompatActivity {
+
+    private static final int RESULT_IMG = 0;
+    ImageView ivSignUpProfilePic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        ivSignUpProfilePic = findViewById(R.id.ivSignUpProfilePic);
     }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+                ivSignUpProfilePic.setImageBitmap(selectedImage);
+                ivSignUpProfilePic.setScaleType(ImageView.ScaleType.CENTER);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(this, "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Toast.makeText(this, "You haven't picked Image",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void getProfilePic(View v) {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, RESULT_IMG);
+    }
+
     public void submit(View v){
         Intent intent = new Intent(signUp.this, loginPage.class);
         startActivity(intent);
