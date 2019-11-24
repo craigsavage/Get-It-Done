09ef9a817a1368fb2b62720bef166bccd80457f4
paivi1 <ryujikomaipro@gmail.com>
2019-11-24 16:24:41 -0500

    message

--- app/src/main/java/com/example/getitdone/NewTaskDetails.java
+++ app/src/main/java/com/example/getitdone/NewTaskDetails.java
@@ -2,7 +2,9 @@ package com.example.getitdone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewTaskDetails extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class NewTaskDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task_details);
+    }
+
+    public void goToTaskList(View v){
+        Intent intent = new Intent(this, TaskList.class);
+        startActivity(intent);
+        overridePendingTransition (R.anim.left_slide_in, R.anim.left_slide_out);//Enter new, Exit old
     }
 }
